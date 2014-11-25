b43fd543f87f47a8b578ed03138833d083d8b5fd
tulio.xcrtf@gmail.com <tulio.xcrtf@gmail.com@b87a6b31-5766-8abf-1fb0-06d0b45eef35>
2014-11-25 10:45:19 +0000

    Added buttons < and >.

--- trunk/SysDocAlpha/src/br/sysdoc/telas/FrmPesquisar.java
+++ trunk/SysDocAlpha/src/br/sysdoc/telas/FrmPesquisar.java
@@ -34,10 +34,6 @@ public class FrmPesquisar extends javax.swing.JFrame {
         }
     }
 
-    public void addFolderFiles() {
-
-    }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
